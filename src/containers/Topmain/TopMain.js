e3643e26f84e0c5d9051cef36d1eb1ede20e2fb2
shaunvan119 <shaunvan@live.com.au>
2022-07-19 09:52:10 +1000

    top main section completed

--- src/containers/Topmain/TopMain.js
+++ src/containers/Topmain/TopMain.js
@@ -1,14 +1,22 @@
 import './topMain.css'
+import computer from '../../assets/home/desktop/beautiful-stories.jpg'
 
 
 function TopMain() {
     
     return (
-        <div className="top-main">
-            <h1>Place holder</h1>
-            
-              
-        </div>
+        <div className="top-main-container">
+            <div className="top-main"> 
+            <h1 className="main-h1">Turn website <br/>visitors into<br/>buyers</h1>
+            <p className="main-p">Get it right the first time, dont spend 30K<br/>
+                on a landing page with all the bells and whistles.<br/>
+                From the point your prospect hits your website<br/>
+                you have less then 15 secounds to attract<br/>
+                their attention.</p>   
+            </div>
+            <img src={computer} alt="computer"/>
+       </div>
+
     )
 }
 
