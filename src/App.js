d89a2dd025cc3626dd1371e90eb4f95126c256b4
shaunvan119 <shaunvan@live.com.au>
2022-07-18 15:07:37 +1000

    top main js files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./containers/Header/Header";
+import TopMain from "./containers/Topmain/TopMain";
 
 
 function App() {
   return (
     <div className="App__wrapper">
       <Header/>
+      <TopMain/>
     </div>
   );
 }
---
+++ src/containers/Topmain/TopMain.js
@@ -0,0 +1,15 @@
+import './topMain.css'
+
+
+function TopMain() {
+    
+    return (
+        <div className="top-main">
+            <h1>Place holder</h1>
+            
+              
+        </div>
+    )
+}
+
+export default TopMain
