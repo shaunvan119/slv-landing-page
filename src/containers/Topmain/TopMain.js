4ac996aeb63e13c0993f118aefd7ce924c757a23
shaunvan119 <shaunvan@live.com.au>
2022-07-19 10:38:00 +1000

    added top main container two

--- src/containers/Topmain/TopMain.js
+++ src/containers/Topmain/TopMain.js
@@ -5,8 +5,9 @@ import computer from '../../assets/home/desktop/beautiful-stories.jpg'
 function TopMain() {
     
     return (
-        <div className="top-main-container">
-            <div className="top-main"> 
+        <div className="top-wrapper">
+        <div className="top-main-container-one">
+            <div className="top-main-one"> 
             <h1 className="main-h1">Turn website <br/>visitors into<br/>buyers</h1>
             <p className="main-p">Get it right the first time, dont spend 30K<br/>
                 on a landing page with all the bells and whistles.<br/>
@@ -14,9 +15,26 @@ function TopMain() {
                 you have less then 15 secounds to attract<br/>
                 their attention.</p>   
             </div>
-            <img src={computer} alt="computer"/>
+            
+            <img src={computer} alt="computer"/>  
        </div>
 
+       <div className="top-main-container-one">
+            <img src={computer} alt="computer"/>  
+            <div className="top-main-two"> 
+            <h1 className="main-h1-two">Fully responsive<br/> website with<br/>purpose</h1>
+            <p className="main-p">We will design a fully responsive website that will<br/>
+            convert traffic into buyers, your website has one job,<br/>
+            generate leads. Web designers are great at making<br/>
+            things pretty but they can not build you a website<br/>
+            that can attract buyers, typically a web designer<br/>
+            doesn't have a background in the psychology of<br/>
+            a consumer's buying behavior to attract buyers.<br/>
+            Your website needs to have emotional triggers to<br/>
+            convert online traffic to hit that submit or buy button.</p>   
+            </div>    
+       </div>
+    </div>
     )
 }
 
